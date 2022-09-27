9c016611e274a09c5fb5000d1c7e67d582526531
Heying778 <yingyinghe2022@gmail.com>
2022-09-27 15:50:13 +0100

    tried hard but MVP not finished

--- script.js
+++ script.js
@@ -1,27 +1,77 @@
-const form = document.querySelector('form');
-const input = document.getElementById('text').placeholder;
-const button = document.getElementById("enter");
-const list = document.getElementById("list");
-const deleteButton=document.createElement("button");
-const item = document.getElementById('to-do')
+// const form = document.querySelector('form');
+// // const input = document.getElementById('text').placeholder;
+// const button = document.getElementById("enter");
 
+// const deleteButton=document.createElement("button");
+// const item = document.getElementById('to-do')
 
-//adding buttom here
+const enter = document.querySelector("#enter");
+const input = document.querySelector("#new-todo");
+const list = document.getElementById("#list");
+
+//delete botton
+const deleteFun = function(){
+    const listItem = this.parentNode;
+    const currentList = listItem.parentNode;
+    currentList.removeChild(listItem);
+}
+
+//check box
+const checkBoxFun = function(event){
+    if(event.currentTarget.checked){
+        alert('checked');
+    }else{
+        alert('not checked');
+    }
+}
 
 button.addEventListener("click", ()=> {
-    const 
+        const newInput = input.value;
+        const listItem = document.createElement("li");
+        listItem.innerText = newInput;
+    
+        // form.addEventListener('submit', event =>{
+        //     event.preventDefault();
+        // })
+        list.appendChild(listItem);
+
+        const deleteButton = document.createElement("button");
+        deleteButton.id="delete";
+        const deleteButtonText = document.createTextNode("delete");
+        deleteButton.appendChild(deleteButtonText);
+        listItem.appendChild(deleteButton);
+
+        const checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+        listItem.appendChild(checkBox);
+        checkBox.addEventListener("edit",checkBoxFun);
+
+        deleteButton.addEventListener('click',deleteFun);
 })
 
 
-button.addEventListener("click", (taskString) =>{
-    const listItem = document.createElement("li");
-    label.innerText=taskString;
+
+//add task
+
+ 
 
 
 
-} )
+	//Create a new list item with the text from the #new-task:
+	// var listItem=createNewTaskElement(taskInput.value);
 
-list.appendChild(listItem2);
+
+
+
+
+
+
+// button.addEventListener("click", (taskString) =>{
+//     const listItem = document.createElement("li");
+//     label.innerText=taskString;
+// } )
+
+// list.appendChild(listItem2);
 
 
 // const listItem2 = document.createElement("li");
