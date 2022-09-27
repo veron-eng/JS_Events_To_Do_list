7d37f4ebe8921b621c6930f03e3bbe8272934e17
Veron Sundaram <veronsundaram13@gmail.com>
2022-09-27 14:29:11 +0100

    tried to add task

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 const form = document.querySelector('form');
+const input = document.getElementById('text').placeholder;
+const button = document.getElementById("enter");
+const list = document.getElementById("list");
+const deleteButton=document.createElement("button");
+const item = document.getElementById('to-do')
+
 
 //adding buttom here
 
-const EnterButtom = document.getElementById("enter");
+button.addEventListener("click", ()=> {
+    const 
+})
 
-EnterButtom.addEventListener("click", () =>{
 
-} )// 
+button.addEventListener("click", (taskString) =>{
+    const listItem = document.createElement("li");
+    label.innerText=taskString;
 
-const listItem2 = document.createElement("li");
-listItem2.innerText = "querySelectorAll() - select all elements that match a query";
 
-const listItem3 = document.createElement("li");
-listItem3.innerText = "convertToSpaceMonkey() - use your space monkey ray-gun to convert all elements to space-monkeys";
 
-// appendChild
-const list = document.querySelector('.list');
+} )
 
-list.appendChild(listItem1); 
-list.appendChild(listItem2); 
-list.appendChild(listItem3); 
+list.appendChild(listItem2);
 
-//remove Child
-list.removeChild(list.lastChild);
+
+// const listItem2 = document.createElement("li");
+// //listItem2.innerText = "querySelectorAll() - select all elements that match a query";
+
+// const listItem3 = document.createElement("li");
+// listItem3.innerText = "convertToSpaceMonkey() - use your space monkey ray-gun to convert all elements to space-monkeys";
+
+// // appendChild
+// const list = document.querySelector('.list');
+
+// list.appendChild(listItem1); 
+// list.appendChild(listItem2); 
+// list.appendChild(listItem3); 
+
+// //remove Child
+// list.removeChild(list.lastChild);
